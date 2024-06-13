edd2c1f63e641faf17877cbbb9467d73139f5d33
GetachewAbebe <gechachin@gmail.com>
2024-06-13 16:52:02 +0300

    Feature Extraction Updated

--- utils/feature_extraction.py
+++ utils/feature_extraction.py
@@ -2,6 +2,9 @@ from haversine import Unit
 import haversine as hs
 import pandas as pd
 from typing import List
+from datetime import datetime as dtime
+import datetime
+
 
 # from scripts.setting_logs import get_rotating_log
 
@@ -127,3 +130,32 @@ class FeatureExtraction:
                     upcoming = pd.to_datetime(
                         f"{original_year}-{upcoming.month}-{upcoming.day}")
                 return previous, upcoming
+
+class WeatherAPIConnector():
+    def __init__(self) -> None:
+        self.URL = "https://api.weatherbit.io/v2.0/history/"
+        self.visited_dates = {}
+
+    def make_weather_api_request(self,endpoint, lat, lng, start_date, end_date):
+        try:
+            pass
+        except Exception as e:
+            pass
+
+    def get_daily_weather(self, df_str_date, lat, lng):
+        dt = dtime.strptime(df_str_date, '%Y-%m-%d %H:%M:%S').date()
+        start_date = f"{dt.year}-{dt.month}-{dt.day}"
+        end_dt = dt + datetime.timedelta(days=1)
+        end_date = f"{end_dt.year}-{end_dt.month}-{end_dt.day}"
+        if start_date not in self.visited_dates:
+            self.visited_dates[start_date] = "PERC"
+            return 1
+        else:
+            return self.visited_dates[start_date]
+        
+
+if __name__ == "__main__":
+    w = WeatherAPIConnector()
+    # print(w.make_request(endpoint="daily",data="SKMCK"))
+    w.get_daily_weather("2021-01-01 07:28:04")
+    w.get_daily_weather("2021-01-01 07:28:04")
